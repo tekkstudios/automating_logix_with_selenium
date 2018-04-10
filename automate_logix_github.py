3d8841bdc04df36400d43bc34e414477b52510ea
Erick Eugene Xavier Wright <wright15@mail.gvsu.edu>
2018-04-10 15:27:29 -0400

    Add files via upload
    Added additional functionality to post validate and set offers to ready to deploy.

--- automate_logix_github.py
+++ automate_logix_github.py
@@ -33,9 +33,53 @@ def credentials():
     driver.implicitly_wait(3)
     click_login.click(loginbox).perform()
     driver.implicitly_wait(15)
+	    time.sleep(2)
+    driver.execute_script('document.getElementById("tab2").click();')
+    time.sleep(2)
 
-"""Main funtion."""
+def cycle():
+    """Cycle a list of offers."""
+    print("List of actions:\n Postvalidate,\n Readytodeploy,\n Neither\n")
+    action = input("What action do you want to take?(enter string):")
+    offer_list = tuple(open("C:\\Users\\108128\\Desktop\\offerstoautomate.txt", "r"))
+    for i in offer_list:
+        offerid = numconvert.convert(i)
+        enteroffer = ActionChains(driver)
+        enteroffer.send_keys_to_element('#searchterms', offerid) \
+                  .perform()
+        driver.execute_script('document.getElementById("search").click();')
+        time.sleep(2)
+        driver.execute_script('toggleDropdown()')
+        if action[0] == 'p' or action[0] == 'P':
+            postvalidate()
+        elif action[0] == 'r' or action[0] == 'R':
+            readytodeploy()
+        else:
+            print("you choose to neither post validate nor ready to deploy...\n\nprogram exiting\n")
+            time.sleep(2)
+            exit()
+        driver.execute_script('document.getElementById("tab2").click();')
+        time.sleep(2)
+
+def postvalidate():
+    """Does post validation."""
+    status = driver.find_element_by_css_selector('#generalbody > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2) > span:nth-child(1)').text
+    if status == 'Pre-validate':
+        driver.execute_script('document.getElementById("postValidate").click();')
+        time.sleep(2)
+        return
+    else:
+        time.sleep(2)
+    
+def readytodeploy():
+    """Does ready to Deploy."""
+    postvalidate()
+    driver.execute_script('document.getElementById("readyToDeploy").click();')
+    time.sleep(2)
+
 def main():
+    """Main funtion."""
     credentials()
+    cycle()
 
 main()
